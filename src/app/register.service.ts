a08b236674bca5a847047d6a6460e65ba99d9d26
Bernáth Máté <bernath.mate.new@gmail.com>
2018-07-18 16:00:15 +0200

    Register-service is added, it is contains the register() method.

---
+++ src/app/register.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RegisterDetails } from './register-details';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  constructor(private http: HttpClient) { }
+
+  register(registerDetails: RegisterDetails): Observable<void> {
+    return this.http.post<void>('api/register', {
+      email: registerDetails.email,
+      password: registerDetails.password,
+      confirmationPassword: registerDetails.confirmationPassword
+    });
+  }
+}
