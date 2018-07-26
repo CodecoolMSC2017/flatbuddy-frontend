45f613412b32cca5c2c1b9d4eae11fb217274a36
GaborMartin <gabormartin94@gmail.com>
2018-07-26 12:53:49 +0200

    Implemented updateProfileDetails method

--- src/app/user-profile.service.ts
+++ src/app/user-profile.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { ChangePassword } from './change-password-details';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,19 @@ export class UserProfileService {
 
   constructor(private http: HttpClient) { }
 
-  
+  public updateProfileDetails(user: any, changePw: ChangePassword): Observable<void> {
+    return this.http.post<void>('/api/user/profile-update', {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      gender: user.gender,
+      age: user.age,
+      description: user.description,
+      destination: user.destination,
+      isFlatmate: user.isFlatmate,
+      oldPw: changePw.oldPw,
+      newPw: changePw.newPw,
+      confirmationPw: changePw.confirmartionPw 
+    });
+    }
 }
