34f74716c2f839a27a1bccc95eec0af01f1f4dd5
GaborMartin <gabormartin94@gmail.com>
2018-07-27 13:49:29 +0200

    Added errorMessage, showError fields in user-profile-component. Added showError span in html. Added .error red color in css.

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {ChangePassword} from '../change-password-details'
 import { User } from '../user';
 import { UserProfileService } from '../user-profile.service';
-import { error } from 'util';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-user-profile',
@@ -14,11 +15,16 @@ export class UserProfileComponent implements OnInit {
 
   user: User = new User();
 
+  errorMessage: String;
+
+  showError: boolean = false;
+
   enabled: boolean = true;
 
   changePw: ChangePassword = new ChangePassword();
 
-  constructor(private userProfileService: UserProfileService, private authService: AuthService) { }
+  constructor(private userProfileService: UserProfileService,
+    private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
     this.authService.getAuth().subscribe(user => this.user = user);
@@ -29,11 +35,23 @@ export class UserProfileComponent implements OnInit {
     this.enabled = false;
   }
 
+  handleError() {
+    this.showError = true;
+    this.errorMessage = "Old password does not match! You have been logged out in 5 secs!";
+  }
+
+  navigateToLogin() : void {
+    this.router.navigate(["login"]);
+  }
+
   save() : void {
     this.enabled = true;
     this.userProfileService.updateProfileDetails(this.user, this.changePw)
     .subscribe(console.log, error => {
-      console.log(error);
+      if (error.status == 401) {
+        this.handleError();
+        setTimeout(() => {this.navigateToLogin()}, 5000);
+      }
     });
   }
 }
