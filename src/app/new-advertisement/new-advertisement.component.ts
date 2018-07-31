7cf1a60193048fd60af425282eb28e675a8a7e60
peti120 <peti59550@gmail.com>
2018-07-31 11:23:06 +0200

    Error handling in new advertisement added

--- src/app/new-advertisement/new-advertisement.component.ts
+++ src/app/new-advertisement/new-advertisement.component.ts
@@ -11,14 +11,36 @@ import { Router } from '@angular/router';
 export class NewAdvertisementComponent implements OnInit {
 
   advertisement: Advertisement = new Advertisement;
+
+  showError: boolean = false;
+  errorMessage: string;
+
   constructor(private newAdService: NewAdvertisementService, private router: Router) { }
 
   ngOnInit() {
   }
   createNewAd(){
-    this.newAdService.createNewAd(this.advertisement).subscribe(() => {
-      this.router.navigate(['advertisements']);
+    const fieldsComplete = this.checkFields();
+    if (!fieldsComplete) {
+      this.newAdService.createNewAd(this.advertisement).subscribe(() => {
+        this.router.navigate(['advertisements']);
+    },
+      (error) => {
+        this.showError = true;
+        this.errorMessage = error.error.message;
     });
+  }
+}
+  private checkFields(): boolean {
+    const result = !this.advertisement.city || this.advertisement.cost < 1 || 
+    !this.advertisement.country || !this.advertisement.description || this.advertisement.size < 1 ||
+    !this.advertisement.state || !this.advertisement.street || !this.advertisement.type || !this.advertisement.zipCode;
+
+    if (result) {
+      this.showError = true;
+      this.errorMessage = "Please fill out each inputs!";
+      return result;
+    }
   }
 
 }
