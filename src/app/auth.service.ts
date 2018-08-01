56328d3ccaeb29d1577c1cc3c2a8277b72b14f18
GaborMartin <gabormartin94@gmail.com>
2018-08-01 16:37:50 +0200

    Implemented getGoogleAuth, deleteGoogleAuth functions

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 
 import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 import { LoginDetails } from './login-details';
 import { User } from './user';
 import { isUndefined } from 'util';
+import { GoogleAuthService } from 'ng-gapi';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private googleAuthService: GoogleAuthService) { }
 
   getAuth(loginDetails?: LoginDetails): Observable<User> {
     const httpOptions = {};
@@ -25,5 +26,23 @@ export class AuthService {
 
   deleteAuth(): Observable<void> {
     return this.http.delete<void>('/api/auth');
+  }
+
+  getGoogleAuth(): Observable<User> {
+    return this.googleAuthService.getAuth()
+      .flatMap(googleAuth => from(googleAuth.signIn()))
+      .flatMap(googleUser => {
+        return this.http.post<User>('/api/auth', {idToken: googleUser.getAuthResponse().id_token});
+      });
+  }
+
+  deleteGoogleAuth(): void {
+    this.googleAuthService.getAuth().subscribe((auth) => {
+      try {
+        auth.signOut();
+      } catch (e) {
+        console.error(e);
+      }
+    });
   }
 }
