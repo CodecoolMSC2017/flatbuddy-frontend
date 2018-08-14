d9f3ddfdf1389477c803e1107b6d6aad0e3b3252
GaborMartin <gabormartin94@gmail.com>
2018-08-14 14:43:59 +0200

    Implemented sendMessage function, add option input field, information container in html, added color styling

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+import { Router } from '@angular/router';
+import { Message } from '../message';
+import { AuthService } from '../auth.service';
+import { User } from '../user';
+import { Match } from 'src/app/match';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-message',
@@ -7,9 +14,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private msgService: MessageService, private router: Router, private authService: AuthService) { }
+
+  newMessage: Message = new Message();
+  showError: boolean = false;
+  showInfo: boolean = false;
+  errorMessage: string;
+  information: string;
+  user: User = new User();
+  users$: User[];
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(user => {
+      this.msgService.getPeople(user.id).subscribe(users => {
+        this.user = user;
+        this.users$ = users;
+      })
+    })
+  }
+
+  sendNewMessage() {
+    if (this.checkMessageFormFields) {
+      this.msgService.sendMessage(this.newMessage).subscribe(() => {
+        this.showError = false;
+        this.showInfo = true;
+        this.information = "Message sent!";
+        this.newMessage.receiverId = null;
+        this.newMessage.subject = "";
+        this.newMessage.content = "";
+      },
+      (error) => {
+          this.showInfo = false;
+          this.showError = true;
+          this.errorMessage = error.error.message;
+      });
+    }
+  }
+
+  private checkMessageFormFields() : boolean {
+    const result = !this.newMessage.content || !this.newMessage.subject || !this.newMessage.receiverId;
+
+    if (result) {
+      this.showError = true;
+      this.errorMessage = "Please fill out each fields!";
+      return result;
+    }
   }
 
 }
