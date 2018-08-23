0ae460744586fd8212c49e2dea83fdb2aef236bb
peti120 <peti59550@gmail.com>
2018-08-23 08:43:36 +0200

    new message timer implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { interval } from 'rxjs';
 import { NotificationService } from './notification.service';
 import { Notification } from './notification';
 import { AuthService } from './auth.service';
+import { MessageService } from './message.service';
 
 @Component({
   selector: 'app-root',
@@ -14,15 +15,20 @@ export class AppComponent {
   title = 'app';
 
   notificationTimer = interval(5000);
+  messageTimer = interval(5000);
+
   notifications: Notification[]=[];
+  messages: any;
+
   loggedUser;
   
 
-  constructor(private router: Router, private notificationService: NotificationService, private auth: AuthService) {
+  constructor(private router: Router, private notificationService: NotificationService, private auth: AuthService, private messageService: MessageService) {
     
    }
    ngOnInit(){
     this.notificationTimerStart();
+    this.messageTimerStart();
    }
    isHaveUnseen(notifications): boolean{
     for (var item of notifications) {
@@ -52,5 +58,27 @@ export class AppComponent {
         }
       }));
      });
+  }
+  messageTimerStart(){
+    this.messageTimer.subscribe(() => {
+      if(this.router.url == "/login" || this.router.url == "/register"){
+        return;
+      }
+      this.messageService.getReceivedMessages().subscribe(messages => {
+        this.messages = messages;
+        let el = document.getElementById('messageIcon');
+        if(this.isHaveUnseen(messages)){
+
+          console.log('there is an unseen message');
+          if(el != undefined){
+            el.style.color = "red";
+          }
+        }else{
+          if(el != undefined){
+            el.style.color = "";
+          }
+        }
+      })
+    });
   }
 }
